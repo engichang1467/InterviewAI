9c241e0d8226d69e47c63dcfa1b325283288a452
Michael Chang <mchang0926@gmail.com>
2021-01-17 22:28:54 -0800

    add database for interviewQs

---
+++ database/interviewQs.sql
@@ -0,0 +1,19 @@
+create table interviewQs (
+	id integer primary key,
+	question VARCHAR not null
+);
+
+select * from interviewQs;
+
+insert into interviewQs values (1, 'Tell me about yourself');
+insert into interviewQs values (2, 'Why do you want this job?');
+insert into interviewQs values (3, 'What are your greatest strength?');
+insert into interviewQs values (4, 'What would you consider to be your weaknesses?');
+insert into interviewQs values (5, 'Tell me about a time you demonstrated leadership skills?');
+insert into interviewQs values (6, 'Why are you interested in this internship/company/industry, and what skills or experiences do you have hope to gain?');
+insert into interviewQs values (7, 'What is the best team you have ever been part of, and why? What is your idea team?');
+insert into interviewQs values (8, 'Tell us about an assignment or project from start to finish - What went well, and what would you have done differently?');
+insert into interviewQs values (9, 'What is one challenge you have faced, an how did you overcome it?');
+insert into interviewQs values (10, 'Tell us about a time you had to learn something completely new.');
+insert into interviewQs values (11, 'Can you tell us anout a project or accomplishment you are proud of and why?');
+insert into interviewQs values (12, 'Do you have any questions for us?');
